459af6edf5912398b3f5d14c36340f14876b5183
ChoiWheatley <chltmdgus604@naver.com>
2023-03-23 22:49:01 +0900

    [www] canvas로 변경

--- index.js
+++ index.js
@@ -1,14 +1,89 @@
-import { Universe } from "game-of-life-wasm";
+import { Universe, Cell } from "game-of-life-wasm";
+import { memory } from "game-of-life-wasm/game_of_life_wasm_bg";
 
-const pre = document.getElementById("game-of-life-canvas");
-const universe = Universe.new(128, 128);
+const CELL_SIZE = 5; // px
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
 
-const renderLoop = () => {
-  pre.textContent = universe.render();
-  universe.tick();
+const width = 256;
+const height = 256;
+const universe = Universe.new(width, height);
 
+// Give the canvas room for all of our cells and a 1px border
+// around each of them.
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext("2d");
+
+const fps = 12;
+const fpsInterval = 1000 / 12;
+var now, then, elapsed;
+
+const getIndex = (row, column) => {
+  return row * width + column;
+};
+
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; ++row) {
+    for (let col = 0; col < width; ++col) {
+      const idx = getIndex(row, col);
+
+      ctx.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  ctx.stroke();
+};
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  // Vertical lines
+  for (let i = 0; i <= width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  // Horizontal lines
+  for (let j = 0; j <= height; ++j) {
+    ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
+};
+
+const renderLoop = (newtime) => {
   requestAnimationFrame(renderLoop);
+
+  now = newtime;
+  elapsed = now - then;
+  if (elapsed > fpsInterval) {
+    then = now - (elapsed % fpsInterval);
+    universe.tick();
+
+    drawGrid();
+    drawCells();
+  }
 };
 
 // Start the rendering process
+then = window.performance.now();
+drawGrid();
+drawCells();
 requestAnimationFrame(renderLoop);
